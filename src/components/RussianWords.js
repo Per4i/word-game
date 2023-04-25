c08c7fca1c84b2865eeb512679b74786c03f0b45
Per4i <pugovkin90.2@gmail.com>
2023-04-26 01:33:34 +0400

    updated className in RW

--- src/components/RussianWords.js
+++ src/components/RussianWords.js
@@ -5,7 +5,12 @@ const RussianWords = (props) => {
   return (
     <ul className={styles.list}>
       {props.russianWords.map((word, index) => (
-        <WordItem key={index} word={word} handleClick={() => props.handleRussianWordClick(word)} />
+        <WordItem
+          className={`russian-word ${word === props.selectedRussianWord ? styles.selected : ''}`}
+          key={index}
+          word={word}
+          handleClick={() => props.handleRussianWordClick(word)}
+        />
       ))}
     </ul>
   );
